0e465204745cee0f98b5f0a39e839067098a5fd9
Celtenn <enescelten@gmail.com>
2024-12-13 09:42:46 +0300

    Update client.c

--- client.c
+++ client.c
@@ -4,22 +4,10 @@
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	n;
 	int	result;
 
 	i = 0;
-	n = 1;
 	result = 0;
-	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-		{
-			n = -1;
-		}
-		i++;
-	}
 	while(str[i] >= '0' && str[i] <= '9' && str[i])
 	{
 		result *= 10;
